f32f42433f796f0714dc5b440c66e64305f29234
Najeeb222 <Najeeb13750@gmail.com>
2025-01-22 19:32:34 +0500

    create custom card and custom detail card

---
+++ src/components/CustomCard/CustomCard.tsx
@@ -0,0 +1,86 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
+import { COLORS } from "@muc/constants";
+import { Box, Rating, Stack } from "@mui/material";
+
+const CustomCard = (props: any) => {
+  return (
+    <Card
+      sx={{
+        maxWidth: 195,
+        ":hover": {
+          scale: "1.1",
+          transition: "all ease 0.5s",
+        },
+      }}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="194"
+          width={"100%"}
+          image={props.img}
+          alt="green iguana"
+          sx={{ position: "relative", objectFit: "cover" }}
+        />
+        <Box
+          position={"absolute"}
+          right={"5%"}
+          top={"55%"}
+          bgcolor={"red"}
+          width={"37px"}
+          height={"20px"}
+          sx={{
+            bgcolor: COLORS.white.darkwhite,
+            opacity: ".8",
+            justifyContent: "center",
+            textAlign: "center",
+            borderRadius: "5px",
+          }}
+        >
+          <Typography variant="body1" fontWeight={700}>
+            +4
+          </Typography>
+        </Box>
+        <CardContent sx={{ padding: "10px", gap: "10px" }}>
+          <Stack
+            direction={"row"}
+            sx={{ justifyContent: "space-between", pb: "5px" }}
+          >
+            <Typography sx={{ color: COLORS.primary.main, fontSize: "18px" }}>
+              {props.name}
+            </Typography>
+            <Typography sx={{ color: COLORS.primary.main, fontSize: "18px" }}>
+              {props.age}
+            </Typography>
+          </Stack>
+          <Typography variant="body2" color={COLORS.gray.lightGray}>
+            {props.profession}
+          </Typography>
+          <Stack
+            direction={"row"}
+            sx={{ justifyContent: "space-between", py: "3px" }}
+          >
+            <Typography variant="body2" color={COLORS.gray.lightGray}>
+              {props.location}
+            </Typography>
+            <Box
+              component={"img"}
+              src={props.countryFlag}
+              sx={{ width: "20px", height: "14px" }}
+            />
+          </Stack>
+          <Rating
+            value={props.rating}
+            size="small"
+            sx={{ color: COLORS.primary.main }}
+          />
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+export default CustomCard;
---
+++ src/components/CustomDetailCard/CustomDetailCard.tsx
@@ -0,0 +1,44 @@
+import { Box, Paper, Stack, Typography } from "@mui/material";
+import React, { ReactNode } from "react";
+
+interface CustomDetailCardProps {
+  icon: ReactNode;
+  iconStyles: React.CSSProperties;
+  backgroundColor: string;
+  textColor: string;
+  title: string;
+}
+function isReactElement(value: React.ReactNode): value is React.ReactElement {
+  return React.isValidElement(value);
+}
+
+const CustomDetailCard: React.FC<CustomDetailCardProps> = (props) => {
+  const { icon, iconStyles, backgroundColor, textColor, title } = props || {};
+  return (
+    <Stack
+      component={Paper}
+      sx={{
+        width: "175px",
+        borderRadius: "16px",
+        justifyContent: "space-between",
+      }}
+    >
+      <Box sx={{ padding: "16px", maxHeight: "88px" }}>
+        {isReactElement(icon) && React.cloneElement(icon, { sx: iconStyles })}
+      </Box>
+      <Typography
+        variant="body1"
+        sx={{
+          bgcolor: backgroundColor,
+          padding: " 14px 16px",
+          borderRadius: "0 0 4px 4px",
+          color: textColor,
+        }}
+      >
+        {title}
+      </Typography>
+    </Stack>
+  );
+};
+
+export default CustomDetailCard;
