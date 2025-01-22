9101eb38218211963c722c4112706a4982647851
Najeeb222 <Najeeb13750@gmail.com>
2025-01-22 19:36:09 +0500

    create user Profile

---
+++ src/modules/ManageHome/components/UserProfile/UserProfile.tsx
@@ -0,0 +1,123 @@
+import { COLORS } from "@muc/constants";
+import {
+  AccountBox,
+  NotificationAdd,
+  PhotoAlbum,
+  Settings,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Paper,
+  Rating,
+  Stack,
+  Switch,
+  Typography,
+} from "@mui/material";
+
+const UserProfile = () => {
+  return (
+    <>
+      <Box
+        component={Paper}
+        width={"380px"}
+        sx={{ padding: "21px 0", height: "385px", mb: "10px" }}
+      >
+        <Stack
+          direction={"row"}
+          sx={{
+            alignItems: "center",
+            justifyContent: "space-between",
+            px: "20px",
+          }}
+        >
+          <Button
+            variant="outlined"
+            sx={{
+              minWidth: 0,
+              minHeight: 0,
+              paddingX: "9px",
+              height: "25px",
+              borderRadius: "32px",
+              fontSize: "13px",
+              color: COLORS.red.main,
+              borderColor: COLORS.red.main,
+              ":hover": {
+                bgcolor: COLORS.red.main,
+                color: COLORS.white.main,
+              },
+            }}
+          >
+            Logout
+          </Button>
+          <Box display="flex" alignItems="center" gap={2}>
+            <Switch />
+          </Box>
+        </Stack>
+
+        <Stack
+          sx={{
+            alignItems: "center",
+            justifyContent: "center",
+            paddingTop: "30px",
+          }}
+        >
+          <Box
+            component={"img"}
+            src="/assets/images/UserImg.jpg"
+            alt="UserProfile"
+            sx={{ width: "140px", height: "140px", borderRadius: "50%" }}
+          />
+          <Typography
+            sx={{ color: COLORS.secondary.main, pt: "23px", fontSize: "24px" }}
+          >
+            UserName.........23
+          </Typography>
+          <Rating
+            size="medium"
+            sx={{ color: COLORS.secondary.main, pb: "15px", pt: "8px" }}
+          />
+        </Stack>
+
+        <Stack direction={"row"} justifyContent={"space-around"}>
+          {UserData.map((item) => (
+            <Stack sx={{ alignItems: "center", justifyContent: "center" }}>
+              <IconButton>{item.icon}</IconButton>
+              <Typography fontSize={"10px"}>{item.title}</Typography>
+            </Stack>
+          ))}
+        </Stack>
+      </Box>
+    </>
+  );
+};
+
+export default UserProfile;
+
+const UserData = [
+  {
+    icon: (
+      <AccountBox sx={{ color: COLORS.secondary.main, fontSize: "32px" }} />
+    ),
+    title: "Edit profile",
+  },
+  {
+    icon: (
+      <PhotoAlbum sx={{ color: COLORS.secondary.main, fontSize: "32px" }} />
+    ),
+    title: "My Photos",
+  },
+  {
+    icon: <Settings sx={{ color: COLORS.secondary.main, fontSize: "32px" }} />,
+    title: "Preferences",
+  },
+  {
+    icon: (
+      <NotificationAdd
+        sx={{ color: COLORS.secondary.main, fontSize: "32px" }}
+      />
+    ),
+    title: "Notifications",
+  },
+];
